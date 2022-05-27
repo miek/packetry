e31c39d3aff96a24f670c42176330224244837a4
Martin Ling <martin-git@earth.li>
2022-05-27 14:05:54 +0100

    First pass at tree model.

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,19 @@ use thiserror::Error;
 
 mod model;
 pub mod row_data;
+use row_data::RowData;
 mod expander;
 
 use std::sync::{Arc, Mutex};
 
-use gtk::gio::ListModel;
-use gtk::glib::Object;
 use gtk::{
     prelude::*,
     ListView,
     Label,
-    TreeExpander,
-    TreeListModel,
-    TreeListRow,
     SignalListItemFactory,
     SingleSelection,
-    Orientation,
 };
 use row_data::GenericRowData;
-use model::GenericModel;
 use expander::ExpanderWrapper;
 
 mod capture;
@@ -31,115 +25,13 @@ use capture::{Capture, CaptureError};
 mod decoder;
 use decoder::Decoder;
 
+mod tree_list_model;
+use tree_list_model::TreeListModel;
+
 mod id;
 mod file_vec;
 mod hybrid_index;
 mod usb;
-
-fn create_view<Item, Model, RowData>(capture: &Arc<Mutex<Capture>>)
-        -> ListView
-    where
-        Model: GenericModel<Item> + IsA<ListModel>,
-        RowData: GenericRowData<Item> + IsA<Object>
-{
-    let model = Model::new(capture.clone(), None);
-    let cap_arc = capture.clone();
-    let tree_model = TreeListModel::new(&model, false, false, move |o| {
-        match (cap_arc.lock(), o.downcast_ref::<RowData>()) {
-            (Ok(mut cap), Some(row)) => match row.child_count(&mut cap) {
-                Ok(0) | Err(_) => None,
-                Ok(_) => Some(
-                    Model::new(cap_arc.clone(), row.get_item())
-                          .upcast::<ListModel>())
-            },
-            _ => None
-        }
-    });
-    let selection_model = SingleSelection::new(Some(&tree_model));
-    let factory = SignalListItemFactory::new();
-    factory.connect_setup(move |_, list_item| {
-        let text_label = Label::new(None);
-        if RowData::CONNECTORS {
-            let expander = ExpanderWrapper::new();
-            list_item.set_child(Some(&expander));
-        } else {
-            let expander = TreeExpander::new();
-            expander.set_child(Some(&text_label));
-            list_item.set_child(Some(&expander));
-        }
-    });
-    factory.connect_bind(move |_, list_item| {
-        let treelistrow = list_item
-            .item()
-            .expect("The item has to exist.")
-            .downcast::<TreeListRow>()
-            .expect("The item has to be a TreeListRow.");
-
-        let row = treelistrow
-            .item()
-            .expect("The item has to exist.")
-            .downcast::<RowData>()
-            .expect("The item has to be RowData.");
-
-        let container = list_item
-            .child()
-            .expect("The child has to exist");
-
-        let text_label = container
-            .last_child()
-            .expect("The child has to exist")
-            .downcast::<Label>()
-            .expect("The child must be a Label.");
-
-        let summary = row.get_summary();
-        text_label.set_text(&summary);
-
-        if RowData::CONNECTORS {
-            let expander_wrapper = container
-                .downcast::<ExpanderWrapper>()
-                .expect("The child must be a ExpanderWrapper.");
-
-            expander_wrapper.set_connectors(row.get_connectors());
-            let expander = expander_wrapper.expander();
-            expander.set_visible(treelistrow.is_expandable());
-            expander.set_expanded(treelistrow.is_expanded());
-            let handler = expander.connect_expanded_notify(move |expander| {
-                treelistrow.set_expanded(expander.is_expanded());
-            });
-            expander_wrapper.set_handler(handler);
-        } else {
-            let tree_expander = container
-                .downcast::<TreeExpander>()
-                .expect("The child must be a TreeExpander.");
-
-            tree_expander.set_list_row(Some(&treelistrow));
-        }
-    });
-    factory.connect_unbind(move |_, list_item| {
-        let container = list_item
-            .child()
-            .expect("The child has to exist");
-
-        if RowData::CONNECTORS {
-            let expander_wrapper = container
-                .downcast::<ExpanderWrapper>()
-                .expect("The child must be a ExpanderWrapper.");
-
-            let expander = expander_wrapper.expander();
-            match expander_wrapper.take_handler() {
-                Some(handler) => expander.disconnect(handler),
-                None => panic!("Handler was not set")
-            };
-        } else {
-            let tree_expander = container
-                .downcast::<TreeExpander>()
-                .expect("The child must be a TreeExpander.");
-
-            tree_expander.set_list_row(None);
-        }
-    });
-    ListView::new(Some(&selection_model), Some(&factory))
-}
 
 #[derive(Error, Debug)]
 pub enum PacketryError {
@@ -173,8 +65,62 @@ fn run() -> Result<(), PacketryError> {
             .title("Packetry")
             .build();
 
-        let listview = create_view::
-            <capture::Item, model::Model, row_data::RowData>(&capture);
+        let tree_model = TreeListModel::new(capture.clone());
+        let selection_model = SingleSelection::new(Some(&tree_model));
+        let factory = SignalListItemFactory::new();
+
+        factory.connect_setup(move |_, list_item| {
+            let expander = ExpanderWrapper::new();
+            list_item.set_child(Some(&expander));
+        });
+
+        factory.connect_bind(move |_, list_item| {
+            let row = list_item
+                .item()
+                .expect("The item has to exist.")
+                .downcast::<RowData>()
+                .expect("The item has to be RowData.");
+
+            let container = list_item
+                .child()
+                .expect("The child has to exist");
+
+            let text_label = container
+                .last_child()
+                .expect("The child has to exist")
+                .downcast::<Label>()
+                .expect("The child must be a Label.");
+
+            let summary = row.get_summary();
+            text_label.set_text(&summary);
+
+            let expander_wrapper = container
+                .downcast::<ExpanderWrapper>()
+                .expect("The child must be a ExpanderWrapper.");
+
+            expander_wrapper.set_connectors(row.get_connectors());
+            let expander = expander_wrapper.expander();
+            expander.set_visible(false);
+            expander.set_expanded(false);
+        });
+
+        factory.connect_unbind(move |_, list_item| {
+            let container = list_item
+                .child()
+                .expect("The child has to exist");
+
+            let expander_wrapper = container
+                .downcast::<ExpanderWrapper>()
+                .expect("The child must be a ExpanderWrapper.");
+
+            let expander = expander_wrapper.expander();
+            match expander_wrapper.take_handler() {
+                Some(handler) => expander.disconnect(handler),
+                None => panic!("Handler was not set")
+            };
+        });
+
+        let listview = ListView::new(Some(&selection_model), Some(&factory));
 
         let scrolled_window = gtk::ScrolledWindow::builder()
             .hscrollbar_policy(gtk::PolicyType::Automatic) // Disable horizontal scrolling
@@ -183,25 +129,7 @@ fn run() -> Result<(), PacketryError> {
             .build();
 
         scrolled_window.set_child(Some(&listview));
-
-        let device_tree = create_view::<capture::DeviceItem,
-                                        model::DeviceModel,
-                                        row_data::DeviceRowData>(&capture);
-        let device_window = gtk::ScrolledWindow::builder()
-            .hscrollbar_policy(gtk::PolicyType::Automatic)
-            .min_content_height(480)
-            .min_content_width(240)
-            .child(&device_tree)
-            .build();
-
-        let paned = gtk::Paned::builder()
-            .orientation(Orientation::Horizontal)
-            .wide_handle(true)
-            .start_child(&scrolled_window)
-            .end_child(&device_window)
-            .build();
-
-        window.set_child(Some(&paned));
+        window.set_child(Some(&scrolled_window));
         window.show();
     });
     application.run_with_args::<&str>(&[]);
---
+++ src/tree_list_model.rs
@@ -0,0 +1,102 @@
+use std::sync::{Arc, Mutex};
+
+use gtk::subclass::prelude::*;
+use gtk::{gio, glib};
+
+use crate::capture::Capture;
+
+glib::wrapper! {
+    pub struct TreeListModel(ObjectSubclass<imp::TreeListModel>) @implements gio::ListModel;
+}
+
+impl TreeListModel {
+    pub fn new(capture: Arc<Mutex<Capture>>) -> Self {
+        let mut model: TreeListModel =
+            glib::Object::new(&[]).expect("Failed to create TreeListModel");
+        {
+            let mut cap = capture.lock().unwrap();
+            model.set_item_count(cap.item_count(&None).unwrap());
+        }
+        model.set_capture(capture);
+        model
+    }
+
+    fn set_item_count(&mut self, count: u64) {
+        self.imp().item_count.set(count.try_into().unwrap());
+    }
+
+    fn set_capture(&mut self, capture: Arc<Mutex<Capture>>) {
+        self.imp().capture.replace(Some(capture));
+    }
+}
+
+mod imp {
+    use std::cell::{Cell, RefCell};
+    use std::sync::{Arc, Mutex};
+
+    use gtk::subclass::prelude::*;
+    use gtk::{prelude::*, gio, glib};
+    use thiserror::Error;
+
+    use crate::capture::{Capture, CaptureError};
+    use crate::row_data::RowData;
+
+    #[derive(Error, Debug)]
+    pub enum ModelError {
+        #[error(transparent)]
+        CaptureError(#[from] CaptureError),
+        #[error("Capture not set")]
+        CaptureNotSet,
+        #[error("Locking capture failed")]
+        LockError,
+    }
+
+    #[derive(Default)]
+    pub struct TreeListModel {
+        pub(super) capture: RefCell<Option<Arc<Mutex<Capture>>>>,
+        pub(super) item_count: Cell<u32>,
+    }
+
+    #[glib::object_subclass]
+    impl ObjectSubclass for TreeListModel {
+        const NAME: &'static str = "TreeListModel";
+        type Type = super::TreeListModel;
+        type Interfaces = (gio::ListModel,);
+    }
+
+    impl ObjectImpl for TreeListModel {}
+
+    impl ListModelImpl for TreeListModel {
+        fn item_type(&self, _list_model: &Self::Type) -> glib::Type {
+            RowData::static_type()
+        }
+
+        fn n_items(&self, _list_model: &Self::Type) -> u32 {
+            self.item_count.get()
+        }
+
+        fn item(&self, _list_model: &Self::Type, position: u32)
+            -> Option<glib::Object>
+        {
+            let opt = self.capture.borrow();
+            let mut cap = match opt.as_ref() {
+                Some(mutex) => match mutex.lock() {
+                    Ok(guard) => guard,
+                    Err(_) => return None
+                },
+                None => return None
+            };
+            let item = cap.get_item(&None, position as u64).ok()?;
+            let summary = match cap.get_summary(&item) {
+                Ok(string) => string,
+                Err(e) => format!("Error: {:?}", e)
+            };
+            let connectors = match cap.get_connectors(&item) {
+                Ok(string) => string,
+                Err(e) => format!("Error: {:?}", e)
+            };
+            Some(RowData::new(Some(item), summary, connectors)
+                            .upcast::<glib::Object>())
+        }
+    }
+}
